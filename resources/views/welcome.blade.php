98fc775af7d10852d22abb298631b70d8d45202a
davidearbola <davidearbola@gmail.com>
2024-06-27 15:56:48 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primi passi Laravel</title>
+</head>
+
+<body>
+    <h1>HELLO WORLD LARAVEL!</h1>
+</body>
+
+</html>
